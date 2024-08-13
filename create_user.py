1a1dd407886014661aa76a681512b052c2fd3527
Iton carlos Sangaletti <itoncsangaletti@gmail.com>
2024-08-12 21:16:10 -0300

    Alteração no projeto

--- create_user.py
+++ create_user.py
@@ -6,11 +6,15 @@ with app.app_context():
     # Criação de todas as tabelas
     db.create_all()
 
-    # Criação de um usuário de teste
-    '''username = "admin"
+     #Criação de um usuário de teste
+    username = "admin"
     password = "senha123"
-    admin = User(username=username, password=password)
+    first_name = "Iton"
+    last_name = "Sangaletti"
+    role = "admin"
+    is_admin = True
+    admin = User(username=username, password=password,first_name=first_name,last_name=last_name,role=role,is_admin=is_admin)
     db.session.add(admin)
-    db.session.commit()'''
+    db.session.commit()
 
-    #print(f"Usuário {username} criado com sucesso!")
+    print(f"Usuário {username} criado com sucesso!")
